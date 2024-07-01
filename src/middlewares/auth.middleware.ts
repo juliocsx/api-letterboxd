98d3cb4e74d94ad070cae4e4eb54e06f4edb8ccc
AlyssonMts <alyssonaraujo767@gmail.com>
2024-07-01 01:49:49 -0300

    implementação do 'unauthorizedResponse'para não duplicar códigos

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { Session } from "../models/session.model";
 
+// Configuração do tempo de expiração em milissegundos
+const SESSION_EXPIRATION_TIME = process.env.SESSION_EXPIRATION_TIME || 120000;
+
+// função de resposta de erro. 'unauthorizedResponse' => reduz a duplicação de códigos.
+const unauthorizedResponse = (res: Response, message: string) => {
+  return res.status(401).json({
+    message:"Você não está autorizado a fazer isso"
+  });
+};
+// Middleware de autenticação
 export const authenticationMiddleware = async (
   req: Request,
   res: Response,
@@ -8,41 +18,32 @@ export const authenticationMiddleware = async (
 ) => {
   const userToken = req.headers.token;
 
-  if(!userToken){
-    return res.status(401).json({
-        message: "Você não está autorizado a fazer isso "
-    })
+  if (!userToken) {
+    return unauthorizedResponse(res, "Você não está autorizado a fazer isso");
   }
 
+// Busca a sessão do usuário
   const userSession = await Session.findOne({
     where: { token: userToken, active: true },
   });
 
-  if(!userSession){
-      return res.status(401).json({
-          message: "Você não está autorizado a fazer isso"
-        })
-    }
-    
-    // implementation here
-    const expiredSession = userSession.createdAt
-    const dateSessionMs = expiredSession.getTime()
-    
-    const dateNow = new Date()
-    const dateNowMs = dateNow.getTime()
-     
-     
-    const diferencDate: number = dateNowMs - dateSessionMs
+  if (!userSession) {
+    return unauthorizedResponse(res, "Você não está autorizado a fazer isso");
+  }
 
-    if(diferencDate > 120000){
-        userSession.update({active: false})
-        userSession.save()
-        return res.status(401).json({
-            message: "Você não está autorizado a fazer isso"
-          })
-    }
-    
-    
+// Verifica se a sessão expirou
+
+  const expiredSession = userSession.createdAt;
+  const dateSessionMs = expiredSession.getTime();
+  const dateNow = new Date();
+  const dateNowMs = dateNow.getTime();
+
+  const diferencDate = dateNowMs - dateSessionMs
+
+  if (diferencDate > parseInt('SESSION_EXPIRATION_TIME')) {
+    await userSession.update({ active: false });
+    return unauthorizedResponse(res, "Você não está autorizado a fazer isso");
+  }
+
   next();
-    
 };
