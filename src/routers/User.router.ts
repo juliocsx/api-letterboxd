12450568acdfde69b8df1e2de2954f7a22526108
juliocsx <julio.souza7@hotmail.com>
2024-07-06 14:49:22 -0300

    Adicionei a Autenticação Middle na rota Usuários.

--- src/routers/User.router.ts
+++ src/routers/User.router.ts
@@ -13,7 +13,7 @@ const router = Router();
 router.post("/login", loginUser)
 router.get("/users", authenticationMiddleware, getUsers, );
 router.post("/users", createUser);
-router.patch("/users/:id", updateEmailUser);
-router.delete("/users/:id", deleteUser);
+router.patch("/users/:id",  authenticationMiddleware, updateEmailUser);
+router.delete("/users/:id", authenticationMiddleware, deleteUser);
 
 export default router;
