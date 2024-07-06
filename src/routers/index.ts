ae1325fb1646b14d45eca7b31ebe95d7906f2957
Marcos Gabriel Paz <mgabrielpazdeoliveira@gmail.com>
2024-07-06 16:07:14 -0300

    Sim

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import HelloWordRouter from "./HelloWord.router"
 import UserRouter from "./User.router"
-
+import StreamingRouter from "./Streaming.router"
 const router = Router()
 
 router.use(HelloWordRouter)
 router.use(UserRouter)
+router.use(StreamingRouter)
 
 export default router;
